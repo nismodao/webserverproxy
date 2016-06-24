4a8333a629e8f16dc4a802b3f97d20806c4f6ef5
Shams Ali <ali.s90@outlook.com>
2016-06-23 19:54:35 -0700

    added dockerfile

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ app.post('/api/webhooks/:channel', requestProxy({
   url: 'http://google.mvargeson.ultrahook.com/api/webhooks/:channel'
 }));
 ​
-app.listen(process.env.PORT || 9000);
+app.listen(process.env.PORT || 3000);
 ​
